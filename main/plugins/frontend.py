64047a6f0838740739de345b064dd61f014f011e
MeizCrazyLeader <110610631+MeizCrazyLeader@users.noreply.github.com>
2024-01-09 23:43:08 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -1,6 +1,10 @@
 #Github.com/Vasusen-code
 
-import time, os
+import time
+import os
+
+from telethon import events
+from pyrogram.errors import FloodWait
 
 from .. import bot as Drone
 from .. import userbot, Bot
@@ -8,14 +12,10 @@ from .. import FORCESUB as fs
 from main.plugins.pyroplug import get_msg
 from main.plugins.helpers import get_link, join
 
-from telethon import events
-from pyrogram.errors import FloodWait
-
-from ethon.telefunc import force_sub
-
-ft = f"To use this bot you've to join @{fs}."
+ft = f"To use this bot, you have to join @{fs}."
 
 message = "Send me the message link you want to start saving from, as a reply to this message."
+
 
 @Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
@@ -30,7 +30,7 @@ async def clone(event):
     except TypeError:
         return
     s, r = await force_sub(event.client, fs, event.sender_id, ft)
-    if s == True:
+    if s:
         await event.reply(r)
         return
     edit = await event.reply("Processing!")
@@ -42,8 +42,7 @@ async def clone(event):
         if 't.me/' in link:
             await get_msg(userbot, Bot, Drone, event.sender_id, edit.id, link, 0)
     except FloodWait as fw:
-        return await Drone.send_message(event.sender_id, f'Try again after {fw.x} seconds due to floodwait from telegram.')
+        return await Drone.send_message(event.sender_id, f'Try again after {fw.x} seconds due to floodwait from Telegram.')
     except Exception as e:
         print(e)
-        await Drone.send_message(event.sender_id, f"An error occurred during cloning of `{link}`\n\n**Error:** {str(e)}")
-    
+        await Drone.send_message(event.sender_id, f"An error occurred during cloning of {link}\n\nError: {str(e)}")
